a2a27e185b9ed58fd0e93499611b36a14124c382
Stella Saronja <stella.saronja@gmail.com>
2021-11-14 21:42:49 +0100

    first commit

---
+++ resources/views/partials/nav.php
@@ -0,0 +1,45 @@
+<nav>
+    <h1>Totes : cool</h1>
+    
+    <ul class="nav__ul">
+      <li>
+        <a href="<?php echo BASE_URL; ?>/home">Home</a>
+      </li>
+      <li>
+        <a href="<?php echo BASE_URL; ?>/products">Products</a>
+      </li>
+      <li>
+        <a href="<?php echo BASE_URL; ?>/about">About Us</a>
+      </li>
+      <li>
+        <!-- @todo: set link href -->
+        <a href="">
+          <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 0 47.496 43.536">
+            <g data-name="Group 18" fill="none" stroke="#fff" stroke-linecap="round" stroke-width="5">
+              <g data-name="Ellipse 1">
+                <ellipse cx="17" cy="16.5" rx="17" ry="16.5" stroke="none"/>
+                <ellipse cx="17" cy="16.5" rx="14.5" ry="14"/>
+              </g>
+              <path data-name="Line 1" d="m28.96 25 15 15"/>
+            </g>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <!-- @todo: set link href -->
+        <a href="">
+          <svg xmlns="http://www.w3.org/2000/svg" width="29.29" height="30" viewBox="0 0 29.29 40">
+            <path id="bag" d="M97.816,34.55,95.684,11.137A1.248,1.248,0,0,0,94.439,10h-3.75V7.5A7.455,7.455,0,0,0,88.5,2.192,7.506,7.506,0,0,0,75.689,7.5V10h-3.75a1.248,1.248,0,0,0-1.245,1.137l-2.128,23.41A5,5,0,0,0,73.546,40H92.834a5,5,0,0,0,4.982-5.45ZM88.189,10h-10V7.5a5,5,0,1,1,10,0Z" transform="translate(-68.546)" fill="#fff"/>
+          </svg>
+        </a>
+      </li>
+      <li>
+        <!-- @todo: set link href -->
+        <a href="">
+          <svg xmlns="http://www.w3.org/2000/svg" height="30" viewBox="0 0 45.532 45.532" xml:space="preserve">
+            <path d="M22.766.001C10.194.001 0 10.193 0 22.766s10.193 22.765 22.766 22.765c12.574 0 22.766-10.192 22.766-22.765S35.34.001 22.766.001zm0 6.807a7.53 7.53 0 1 1 .001 15.06 7.53 7.53 0 0 1-.001-15.06zm-.005 32.771a16.708 16.708 0 0 1-10.88-4.012 3.209 3.209 0 0 1-1.126-2.439c0-4.217 3.413-7.592 7.631-7.592h8.762c4.219 0 7.619 3.375 7.619 7.592a3.2 3.2 0 0 1-1.125 2.438 16.702 16.702 0 0 1-10.881 4.013z" fill="#fff"/>
+          </svg>
+        </a>
+      </li>
+    </ul>
+  </nav>
